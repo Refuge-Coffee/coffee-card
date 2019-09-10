61906e1929ee4c106ca31f866eefccde82ebee9e
hiimtaylorjones <taylorjonessoftware@gmail.com>
2019-09-09 22:08:46 -0400

    adding more interactivity to card

--- app/components/interactive-card-search.js
+++ app/components/interactive-card-search.js
@@ -1,12 +1,23 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+
+  firstFour: "",
+  midThree: "",
+  lastFour: "",
+
+  holderName: "CARD HOLDER NAME",
+  holderStatus: "STATUS",
+  holderBalance: "0.00",
 
   init() {
     this._super(...arguments);
   },
 
-  actions: {
-    
-  }
+  fullNumber: computed('firstFour', 'midThree', 'lastFour', function() {
+    return `${this.get('firstFour')}${this.get('midThree')}${this.get('lastFour')}`;
+  }),
+
+
 });
