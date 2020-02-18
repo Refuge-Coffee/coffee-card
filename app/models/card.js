614ebeddecf867649b922de0a09e30e09cb1215b
hiimtaylorjones <taylorjonessoftware@gmail.com>
2020-02-18 13:57:07 -0500

    creating customer model; fixing issues with VSCode linting Ember

--- app/models/card.js
+++ app/models/card.js
@@ -1,6 +1,6 @@
 import Model, { attr } from '@ember-data/model';
 
-export default Model.extend({
+export default class Card extends Model {
   @attr('string') fullNumber;
-  @
-});
+  @attr('number') balanceCents;
+}
