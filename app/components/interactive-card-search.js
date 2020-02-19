2a3ac56e747270baa1359ace576ca79d37f35be5
hiimtaylorjones <taylorjonessoftware@gmail.com>
2020-02-19 15:51:13 -0500

    upgrading syntax on ember component

--- app/components/interactive-card-search.js
+++ app/components/interactive-card-search.js
@@ -5,7 +5,7 @@ import { tracked } from "@glimmer/tracking";
 export default class InteractiveCardSearch extends Component {
 
   @tracked firstFour = "";
-  @tracked midThree = "";
+  @tracked midFour = "";
   @tracked lastFour = "";
 
   @tracked holderName = "CARD HOLDER NAME";
@@ -19,9 +19,10 @@ export default class InteractiveCardSearch extends Component {
 
   @action
   registerValue() {
-    let combined = this.firstFour + this.midThree + this.lastFour;
+    let combined = (this.firstFour + this.midFour + this.lastFour).trim();
     console.log(combined);
-    if (combined === 10) {
+    console.log(combined.length);
+    if (combined.length === 12) {
       this.holderName = "John Apple";
       this.holderStatus = "Platinum";
       this.holderBalance = "99.00";
